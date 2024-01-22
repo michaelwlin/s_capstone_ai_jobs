035222b78663cf7513477acb273f47a559b63a65
Joyce Au <122276911+joyceawt@users.noreply.github.com>
2024-01-22 20:20:33 +0800

    set up mongo DB

--- node-app/app.js
+++ node-app/app.js
@@ -3,8 +3,8 @@
 /*
     SETUP
 */
-var express = require('express');   // We are using the express library for the web server
-var app     = express();            // We need to instantiate an express object to interact with the server in our code
+const express = require('express');   // We are using the express library for the web server
+const app     = express();            // We need to instantiate an express object to interact with the server in our code
 PORT        = 4000;                 // Set a port number at the top so it's easy to change in the future
 
 /*
@@ -21,3 +21,6 @@ app.get('/', function(req, res)                 // This is the basic syntax for 
 app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
     console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
 });
+
+
+// mongo DB 
