c4dc60dd137cc694c576ba68d5efd3ab6f5ab352
Joyce Au <122276911+joyceawt@users.noreply.github.com>
2024-01-28 20:04:49 +0800

    fix: navigation bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,29 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NavigationBar from './components/NavigationBar';
-import FindJobsPage from './pages/FindJobs';
-import HowItWorksPage from './pages/HowItWorks';
-import AboutUsPage from './pages/AboutUs';
-import HomePage from './pages/LandingPage';
+import React, { useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import NavigationBar from './components/NavigationBar'
+import FindJobsPage from './pages/FindJobs'
+import HowItWorksPage from './pages/HowItWorks'
+import AboutUsPage from './pages/AboutUs'
+import HomePage from './pages/LandingPage'
 
 const App = () => {
-
-  useEffect(() => { //TODO: update this with data to be fetched from database
-    fetch('http://localhost:3000')
-    .then((res) => res.json())
+  useEffect(() => {
+    //TODO: update this with data to be fetched from database
+    fetch('http://localhost:3000').then((res) => res.json())
   })
 
   return (
-      <Router>
-          <NavigationBar />
-              <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/find-jobs" element={<FindJobsPage />} />
-              <Route path="/how-it-works" element={<HowItWorksPage />} />
-              <Route path="/about-us" element={<AboutUsPage />} />
-              </Routes>
-      </Router>
-  );
-};
+    <Router>
+      <NavigationBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/find-jobs" element={<FindJobsPage />} />
+        <Route path="/how-it-works" element={<HowItWorksPage />} />
+        <Route path="/about-us" element={<AboutUsPage />} />
+      </Routes>
+    </Router>
+  )
+}
 
-export default App;
-
+export default App
--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -1,30 +1,64 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Button, Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from 'flowbite-react';
-
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { Dropdown, Navbar, NavbarLink, Avatar } from 'flowbite-react'
 
 const NavigationBar = () => {
   return (
-    <Navbar fluid rounded>
-      <Navbar.Brand href="/">
-        <img src="/icons/MatchIQ_logo.svg" className="mr-3 h-6 sm:h-9" alt="Your Logo" />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">MatchIQ</span>
+    <Navbar fluid rounded className="navbar">
+      <Navbar.Brand>
+        <NavLink to="/" as={NavbarLink} className="nav-logo">
+          <img
+            src="/icons/MatchIQ_logo.svg"
+            className="mr-3 h-6 sm:h-9"
+            alt="Your Logo"
+          />
+          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+            MatchIQ
+          </span>
+        </NavLink>
       </Navbar.Brand>
 
-      <Navbar.Collapse>
-        <Navbar.Link href="/" active>
+      <Navbar.Collapse className="nav-menu">
+        <NavLink to="/" as={NavbarLink}>
           Home
-        </Navbar.Link>
-        <Navbar.Link href="/find-jobs">Find Jobs</Navbar.Link>
-        <Navbar.Link href="/how-it-works">How it Works</Navbar.Link>
-        <Navbar.Link href="/about-us">About Us</Navbar.Link>
+        </NavLink>
+        <NavLink to="/find-jobs" as={NavbarLink}>
+          Find Jobs
+        </NavLink>
+        <NavLink to="/how-it-works" as={NavbarLink}>
+          How it Works
+        </NavLink>
+        <NavLink to="/about-us" as={NavbarLink}>
+          About Us
+        </NavLink>
       </Navbar.Collapse>
-      <div className="flex md:order-2">
-        <Button>Get started</Button>
+
+      <div className="flex md:order-2 user-profile">
+        <Dropdown
+          arrowIcon={false}
+          inline
+          label={
+            <Avatar
+              alt="User settings"
+              img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+              rounded
+            />
+          }
+        >
+          <Dropdown.Header>
+            <span className="block text-sm">User Profile</span>
+            <span className="block truncate text-sm font-medium">
+              name@flowbite.com
+            </span>
+          </Dropdown.Header>
+          <Dropdown.Item>Dashboard</Dropdown.Item>
+          <Dropdown.Item>Settings</Dropdown.Item>
+          <Dropdown.Item>Sign out</Dropdown.Item>
+        </Dropdown>
         <Navbar.Toggle />
       </div>
     </Navbar>
-  );
-};
+  )
+}
 
-export default NavigationBar;
+export default NavigationBar
