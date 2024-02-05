bb26867bacab167d78c40bdbbdab8aa63b688f57
McKenzie Bourn <107959606+kenziebourn@users.noreply.github.com>
2024-02-05 15:14:09 -0800

    Revert "Revert "results page route added and search results displayed""

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,5 +2,6 @@ import AboutUs from './AboutUs';
 import FindJobs from './FindJobs';
 import HowItWorks from './HowItWorks';
 import Homepage from './LandingPage'
+import Results from './Results';
 
-export { AboutUs, FindJobs, HowItWorks, Homepage }
+export { AboutUs, FindJobs, HowItWorks, Homepage, Results }
