f51b4ccf3a9faa3b99da178de119db5f128cb378
Joyce Au <122276911+joyceawt@users.noreply.github.com>
2024-02-20 11:45:21 +0800

    Set up form validations use react-hook-form and yup for form validation (#29)
    * feat: use react-hook-form and yup for form validation
    
    * fix: add blank check
    
    * fix: double test wasnt triggering validation
    
    * fix: add trim
    
    * fix: use !! so that we don't have to use trim
    
    * fix: update error messages

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -3,32 +3,79 @@ import { useNavigate } from 'react-router-dom'
 import { Button, TextInput } from 'flowbite-react'
 import { Hero } from '../components'
 import { FaSearch, FaMapPin } from 'react-icons/fa'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+import { useForm } from 'react-hook-form'
+import { maliciousChars } from '../utils/maliciousChars'
 
 const LandingPage = () => {
   const [signedIn, setSignedIn] = useState(false)
   const [keyword, setKeyword] = useState('')
   const [location, setLocation] = useState('')
 
-  const maliciousChars = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=',
-   '|', '\\', '{', '}', '[', ']', ';', ':', '\'', '"', '<', '.', '>'];
+  const schema = yup.object().shape({
+    location: yup
+      .string()
+      .test(
+        'atLeastOne',
+        'Keyword and/or location cannot be empty.',
+        function () {
+          const keyword = this.parent.keyword
+          const location = this.parent.location
 
-  const checkForMaliciousChars = (input) => {
-      return maliciousChars.some(char => input.includes(char));
-  };
+          return !!keyword || !!location
+        },
+      )
+      .test(
+        'maliciousChars',
+        'Special characters are not allowed.',
+        function (value) {
+          return !maliciousChars.some((char) => value?.includes(char))
+        },
+      ),
+    keyword: yup
+      .string()
+      .test(
+        'atLeastOne',
+        'Keyword and/or location cannot be empty.',
+        function () {
+          const keyword = this.parent.keyword
+          const location = this.parent.location
 
-  const navigate = useNavigate();
-  const handleSubmit = (e) => {
-      e.preventDefault();
-      if (!keyword.trim() || !location.trim()) {
-        alert('Keyword and/or location cannot be empty.');
-        return;
-      }
-      if (checkForMaliciousChars(keyword) || checkForMaliciousChars(location)) {
-        alert('Keyword and/or location cannot contain special characters.');
-        return;
-      }
-      navigate('/search-results', { state: { keyword, location } });
-  };
+          return !!keyword || !!location
+        },
+      )
+      .test(
+        'maliciousChars',
+        'Special characters are not allowed.',
+        function (value) {
+          return !maliciousChars.some((char) => value?.includes(char))
+        },
+      ),
+  })
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    clearErrors,
+  } = useForm({
+    resolver: yupResolver(schema),
+  })
+
+  const navigate = useNavigate()
+  const onSubmit = (e) => {
+    navigate('/search-results', { state: { keyword, location } })
+  }
+
+  const onChange = (e, type) => {
+    if (type === 'keyword') {
+      setKeyword(e.target.value)
+    } else {
+      setLocation(e.target.value)
+    }
+    clearErrors(type)
+  }
 
   const uploadResume = () => {}
   const signIn = () => {
@@ -55,25 +102,38 @@ const LandingPage = () => {
     <div className="flex flex-col items-center justify-center min-h-screen">
       <Hero />
       <div className="text-center mt-12">
-        <form className="flex flex-row items-center gap-2" onSubmit={handleSubmit}>
-          <TextInput
-            id="keywords"
-            icon={FaSearch}
-            type="text"
-            placeholder="Keywords"
-            className="w-250"
-            value={keyword}
-            onChange={(e) => setKeyword(e.target.value)}
-          />
-          <TextInput
-            id="location"
-            icon={FaMapPin}
-            type="text"
-            placeholder="Location"
-            className="w-250"
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
-          />
+        <form
+          className="flex flex-row items-start gap-2"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="flex flex-col gap-2">
+            <TextInput
+              {...register('keyword')}
+              id="keywords"
+              icon={FaSearch}
+              type="text"
+              placeholder="Keywords"
+              className="w-250"
+              value={keyword}
+              color={errors.keyword ? 'failure' : 'blue'}
+              helperText={errors.keyword?.message}
+              onChange={(e) => onChange(e, 'keyword')}
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <TextInput
+              {...register('location')}
+              id="location"
+              icon={FaMapPin}
+              type="text"
+              placeholder="Location"
+              className="w-250"
+              value={location}
+              color={errors.location ? 'failure' : 'blue'}
+              helperText={errors.location?.message}
+              onChange={(e) => onChange(e, 'location')}
+            />
+          </div>
           <Button className="w-120" color="blue" type="submit">
             Search
           </Button>
