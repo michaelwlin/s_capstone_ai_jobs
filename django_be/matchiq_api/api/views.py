b0eb6053b7bccc00ea1678d54bb2487ad7c770f4
Nicholai Moguet <moguetn@oregonstate.edu>
2024-02-27 23:30:51 +0700

    Installed django-ratelimiter, updated views.py and urls.py

--- django_be/matchiq_api/api/views.py
+++ django_be/matchiq_api/api/views.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
 from . import scrape_jobs
-
+from ratelimit.decorators import ratelimit
+from ratelimit.exceptions import Ratelimited
+from django.core.exceptions import PermissionDenied
 
 def index(request):
     # Test
@@ -15,22 +17,29 @@ def db(request):
     pass
 
 
+@ratelimit(key='ip', rate='5/m', method='GET', block=True)
 def scrape(request):
-    keywords = request.GET['keywords']
-    location = request.GET['location']
-    limit = request.GET['limit']
+    try:
+        keywords = request.GET['keywords']
+        location = request.GET['location']
+        limit = request.GET['limit']
 
-    response = scrape_jobs.scrape_utility(keywords, location, limit)
+        response = scrape_jobs.scrape_utility(keywords, location, limit)
 
-    formatted_response = {
-        "results": response
-    }
+        formatted_response = {
+            "results": response
+        }
 
-    return JsonResponse(formatted_response)
+        return JsonResponse(formatted_response)
+    except Ratelimited:
+        raise PermissionDenied("This request has been rate-limited.")
 
-
+@ratelimit(key='ip', rate='5/m', method='GET', block=True)
 def scrape_description(request):
-    url = request.GET['url']
-    formatted_response = scrape_jobs.get_description(url)
+    try:
+        url = request.GET['url']
+        formatted_response = scrape_jobs.get_description(url)
 
-    return JsonResponse(formatted_response)
+        return JsonResponse(formatted_response)
+    except Ratelimited:
+        raise PermissionDenied("This request has been rate-limited.")
