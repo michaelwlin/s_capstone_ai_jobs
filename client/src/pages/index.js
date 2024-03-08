ee47e037893332b133bcfaa7e6be84887e98bfb8
Nicholai Moguet <moguetn@oregonstate.edu>
2024-03-09 01:44:10 +0700

    Can search using skills listed under user
    - Go to http://localhost:3000/user-search
    - Toggle "Enhance search with my skills" on
    - Search using keyword and location (fuzzy search not yet implemented)

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -6,5 +6,6 @@ import Results from './Results';
 import Register from './Register';
 import SignIn from './SignIn';
 import Resume from './Resume'
+import UserSearch from './UserSearch'
 
-export { AboutUs, FindJobs, HowItWorks, Homepage, Results, Register, SignIn, Resume }
+export { AboutUs, FindJobs, HowItWorks, Homepage, Results, Register, SignIn, Resume, UserSearch }
--- django_be/matchiq_api/api/urls.py
+++ django_be/matchiq_api/api/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("scrape", views.scrape, name="scrape"),
     path("scrape_desc", views.scrape_description, name="scrape_description"),
     path("upload_resume", csrf_exempt(views.upload_resume), name="upload_resume"),
+    path('match-jobs', views.match_jobs, name='match_jobs'),
 ]
