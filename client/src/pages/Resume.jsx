590f21ab11db90d4da1fe9d54147aa64bbfcdf1d
Joyce Au <122276911+joyceawt@users.noreply.github.com>
2024-03-11 21:55:49 +0800

    fix: increase load time

--- client/src/pages/Resume.jsx
+++ client/src/pages/Resume.jsx
@@ -93,7 +93,7 @@ const Resume = () => {
           : prevHeight
         return calculatedHeight
       })
-    }, 1)
+    }, 1000)
   }, [resume, boardRef, boardRef.current?.scrollHeight])
 
   return (
