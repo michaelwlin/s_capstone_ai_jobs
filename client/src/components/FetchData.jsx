6a784739038c3e9b5cab40dac3847f56c89409dc
kenziebourn <bournm@oregonstate.edu>
2024-03-10 22:57:36 -0700

    add location

--- client/src/components/FetchData.jsx
+++ client/src/components/FetchData.jsx
@@ -2,14 +2,19 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ProgressBar from "./LoadingBar";
 
-function FetchData() {
+function FetchData(props) {
     const [jobs, setJobs] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const { keyword, location } = props;
 
     const getJobs = async () => {
         try {
             const response = await axios.get('http://localhost:4000/api/jobs'); // replace with your API endpoint
-            setJobs(response.data);
+            const filteredJobs = response.data.filter(job => 
+                job.title.toLowerCase().includes(keyword.toLowerCase()) && 
+                job.location.toLowerCase().includes(location.toLowerCase())
+            );
+            setJobs(filteredJobs);
             setIsLoading(false);
         } catch (error) {
             console.error('Failed to fetch jobs:', error);
@@ -34,13 +39,15 @@ function FetchData() {
                     onClick={() => setSelectedJob(job)}>
                         <p className="text-xl"><strong>Title:</strong> {job.title}</p>
                         <p className="text-md"><strong>Company:</strong> {job.company}</p>
+                        <p className="text-md"><strong>Location:</strong> {job.location}</p>
                     </div>
                     ))}
                 </div>
                 {selectedJob && (
                     <div className="w-1/2 p-5 bg-gray-100">
                     <p className="text-xl font-bold mb-2">{selectedJob.title}</p>
-                    <p className="text-md font-semibold mb-3">{selectedJob.company}</p>
+                    <p className="text-md font-semibold mb-1">{selectedJob.company}</p>
+                    <p className="text-sm font-semibold mb-3">{selectedJob.location}</p>
                     <p className="text-sm"><strong>Description:</strong> {selectedJob.description}</p>
                     </div>
                 )}
