cc735617e09f51845c08249a9ac168a3a243cf5c
McKenzie Bourn <107959606+kenziebourn@users.noreply.github.com>
2024-05-16 05:16:34 -0700

    get rid of unused fetchapp and fetchjob (#69)

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,17 +1,13 @@
 import NavigationBar from './NavigationBar'
 import Hero from './Hero'
 import Footer from './Footer'
-import FetchApp from './FetchApp'
 import FetchData from './FetchData'
-import FetchJob from './FetchJob'
 import RequireAuth from './RequireAuth'
 
 export {
   NavigationBar,
   Hero,
   Footer,
-  FetchApp,
   FetchData,
-  FetchJob,
   RequireAuth,
 }
