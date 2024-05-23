90377c7167cdaa8187acf41141a572c2b1592135
Joyce Au <122276911+joyceawt@users.noreply.github.com>
2024-05-23 18:30:23 +0800

    Revert "added tests for api views, checking in test resume (#70)" (#77)
    This reverts commit ed43ed4b905f897ae628f99c7316114ec77408e8.

--- django_be/matchiq_api/api/views.py
+++ django_be/matchiq_api/api/views.py
@@ -10,11 +10,18 @@ import json
 
 logger = logging.getLogger(__name__)
 
+
 def index(request):
+    # Test
     obj = {
         "greeting": "Hello, world!",
     }
     return JsonResponse(obj)
+
+
+def db(request):
+    # Test
+    pass
 
 
 def upload_resume(request):
@@ -24,44 +31,44 @@ def upload_resume(request):
             userID = request.POST['userID']
         except KeyError:
             return JsonResponse({'error': 'No resume file provided'}, status=400)
+
         try:
             data = dt()
             resume_text = data.get_resume_text(resume)
             parsed_resume = data.parse_resume(resume_text)
+
             response = JsonResponse(parsed_resume)
+
             mongo_utils.save_resume_to_mongodb(parsed_resume, userID)
+
             return response
+
         except Exception as e:
             logger.error(f"Error parsing resume: {str(e)}")
             return JsonResponse({'error': f'Error parsing resume: {str(e)}'}, status=500)
+
     return JsonResponse({'error': 'Invalid request method'}, status=400)
 
 
 def scrape(request):
-    if request.method == 'GET':
-        try:
-            keywords = request.GET['keywords']
-            location = request.GET['location']
-            limit = request.GET['limit']
-        except KeyError:
-            return JsonResponse({'error': 'keywords, location, limit are required.'}, status=400)
-        response = scrape_jobs.scrape_utility(keywords, location, limit)
-        formatted_response = {
-            "results": response
-        }
-        return JsonResponse(formatted_response)
-    return JsonResponse({'error': 'Invalid request method'}, status=400)
+    keywords = request.GET['keywords']
+    location = request.GET['location']
+    limit = request.GET['limit']
+
+    response = scrape_jobs.scrape_utility(keywords, location, limit)
+
+    formatted_response = {
+        "results": response
+    }
+
+    return JsonResponse(formatted_response)
 
 
 def scrape_description(request):
-    if request.method == 'GET':
-        try:
-            url = request.GET['url']
-        except KeyError:
-            return JsonResponse({'error': 'url is required.'}, status=400)
-        formatted_response = scrape_jobs.get_description(url)
-        return JsonResponse(formatted_response)
-    return JsonResponse({'error': 'Invalid request method'}, status=400)
+    url = request.GET['url']
+    formatted_response = scrape_jobs.get_description(url)
+
+    return JsonResponse(formatted_response)
 
 
 def proofread(request):
@@ -105,44 +112,27 @@ def enhance(request):
     else:
         return JsonResponse({'error': 'Invalid request method'}, status=405)
 
+def stringify_resume_obj(resume):
+    result_string = ""
+    for key, value in resume.items():
+        if isinstance(value, dict):
+            result_string += stringify_resume_obj(value) + " "
+        else:
+            result_string += str(value) + " "
+    print(result_string)
+    return result_string
+
 
 def wordbank(request):
-    if request.method == 'POST':
-        json_body = json.loads(request.body)
-        logger.info(json_body)
-        if not json_body:
-            return JsonResponse({'error': 'Missing JSON body'}, status=400)
-        try:
-            body = json.loads(request.body.decode("utf-8"))
-        except json.JSONDecodeError:
-            return JsonResponse({'error': 'Invalid JSON format'}, status=400)
-        data = dt()
-        wordbank = data.wordbank(json.dumps(body))
-        return JsonResponse(wordbank)
-    else:
-        return JsonResponse({'error': 'Invalid request method'}, status=405)
-
+    resume = request.body.decode("utf-8")
+    body = json.loads(resume)
+    data = dt()
+    wordbank = data.wordbank(json.dumps(body))
+    return JsonResponse(wordbank)
 
 def get_score(request):
-    if request.method == 'POST':
-        json_body = json.loads(request.body)
-        logger.info(json_body)
-        if not json_body:
-            return JsonResponse({'error': 'Missing JSON body'}, status=400)
-        try:
-            body = json.loads(request.body.decode("utf-8"))
-        except json.JSONDecodeError:
-            return JsonResponse({'error': 'Invalid JSON format'}, status=400)
-        resume = request.body.decode("utf-8")
-        body = json.loads(resume)
-        data = dt()
-        try:
-            resume_text = body['resume_text']
-            job_description = body['job_description']
-        except KeyError:
-            return JsonResponse({'error': 'resume_text, job_description are required.'}, status=400)
-        score = data.get_score(resume_text, job_description)
-        return JsonResponse(score)
-    else:
-        return JsonResponse({'error': 'Invalid request method'}, status=405)
-
+    resume = request.body.decode("utf-8")
+    body = json.loads(resume)
+    data = dt()
+    score = data.get_score(body['resume_text'], body['job_desc'])
+    return JsonResponse(score)
