b3e7ab981abfc20ed29f584389f6ce96b6a3c742
Farsidetfs <wilsoros@oregonstate.edu>
2024-06-03 20:26:13 -0700

    Updated to remove "all users" route.  Removed requests.rest

--- server/routes/users.js
+++ server/routes/users.js
@@ -9,23 +9,23 @@ const {
 const User = require('../models/user')
 const router = express.Router()
 
-router.get('/', async (req, res) => {
-  const users = await User.find().sort('userName')
-  res.send(users)
-})
+// router.get('/', async (req, res) => {
+//   const users = await User.find().sort('userName')
+//   res.send(users)
+// })
 
 router.get("/loggedInData", authenticateAccessToken, async (req, res) => {
-    const user = await User.findById(req.user.userId);
-    if (!user) {
-        return res.status(404).send('User not found');
-    }
-    res.json(user);
+  const user = await User.findById(req.user.userId);
+  if (!user) {
+    return res.status(404).send('User not found');
+  }
+  res.json(user);
 });
 // router.get("/:id", validateID, async (req, res) => {
 router.get("/:id", async (req, res) => {
-    const user = await User.findById(req.params.id);
-    if (!user) return res.status(404).send();
-    res.send(user);
+  const user = await User.findById(req.params.id);
+  if (!user) return res.status(404).send();
+  res.send(user);
 });
 
 router.delete('/:id', async (req, res) => {
@@ -44,24 +44,24 @@ router.delete('/:id', async (req, res) => {
 })
 
 router.put("/:id", async (req, res) => {
-    const { id } = req.params;
-    const updateData = req.body;
+  const { id } = req.params;
+  const updateData = req.body;
 
-    try {
-        const updatedUser = await User.findByIdAndUpdate(id, updateData, { new: true });
-        if (!updatedUser) {
-            return res.status(404).send("User not found.");
-        }
-        res.send(updatedUser);
-    } catch (error) {
-        res.status(400).send("Error updating user: " + error.message);
+  try {
+    const updatedUser = await User.findByIdAndUpdate(id, updateData, { new: true });
+    if (!updatedUser) {
+      return res.status(404).send("User not found.");
     }
+    res.send(updatedUser);
+  } catch (error) {
+    res.status(400).send("Error updating user: " + error.message);
+  }
 });
 router.get("/:id/skills", async (req, res) => {
-    // router.get("/:id/skills", validateID, async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id);
-        if (!user) return res.status(404).send('User not found');
+  // router.get("/:id/skills", validateID, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).send('User not found');
 
     res.json(user.skills)
   } catch (error) {
